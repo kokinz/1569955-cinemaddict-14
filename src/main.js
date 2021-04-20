ee37443cfb306eb02179b69a74a1036dc2507586
kokinz <nes-t@mail.ru>
2021-04-20 22:36:20 +0300

    film-lists

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createFilmListsTemplate} from './view/film-lists.js';
+import {createFilmCardTemplate} from './view/film-card.js';
+
+const FILMS_CARD_COUNTER = 5;
+const EXTRA_FILMS_COUNTER = 2;
 
 const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
@@ -7,3 +12,18 @@ const render = (container, template, place = 'beforeend') => {
 const siteMainElement = document.querySelector('.main');
 
 render(siteMainElement, createSiteMenuTemplate(), 'afterbegin');
+render(siteMainElement, createFilmListsTemplate());
+
+const filmListElement = siteMainElement.querySelector('.films-list__container');
+
+for (let i = 1; i <= FILMS_CARD_COUNTER; i++) {
+  render(filmListElement, createFilmCardTemplate());
+}
+
+const extraFilmListsElements = siteMainElement.querySelectorAll('.films-list--extra .films-list__container');
+
+for (let i = 1; i <= extraFilmListsElements.length; i++) {
+  for (let j = 0; j < EXTRA_FILMS_COUNTER; j++) {
+    render(extraFilmListsElements[j], createFilmCardTemplate());
+  }
+}
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -7,7 +7,13 @@ const createSiteMenuTemplate = () => {
       <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
-  </nav>`;
+  </nav>
+
+  <ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>`;
 };
 
 export {createSiteMenuTemplate};
