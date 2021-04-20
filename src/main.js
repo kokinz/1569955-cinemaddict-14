6422543fd40f1547b51b2c786586569663806d79
kokinz <nes-t@mail.ru>
2021-04-20 23:05:56 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import {createSiteMenuTemplate} from './view/site-menu.js';
 import {createFilmListsTemplate} from './view/film-lists.js';
 import {createFilmCardTemplate} from './view/film-card.js';
+import {createUserRankTemplate} from './view/user-rank.js';
+import {createShowMoreButtonTemplate} from './view/show-more-button.js';
+import {createFilmPopupTemplate} from './view/film-popup.js';
+import {createMoviesCounterTemplate} from './view/movies-counter.js';
 
 const FILMS_CARD_COUNTER = 5;
 const EXTRA_FILMS_COUNTER = 2;
@@ -9,16 +13,23 @@ const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
 
+const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
+const footerStatistics = siteFooterElement.querySelector('.footer__statistics');
 
+render(siteHeaderElement, createUserRankTemplate());
 render(siteMainElement, createSiteMenuTemplate(), 'afterbegin');
 render(siteMainElement, createFilmListsTemplate());
+render(footerStatistics, createMoviesCounterTemplate());
+render(siteFooterElement, createFilmPopupTemplate(), 'afterend');
 
 const filmListElement = siteMainElement.querySelector('.films-list__container');
 
 for (let i = 1; i <= FILMS_CARD_COUNTER; i++) {
   render(filmListElement, createFilmCardTemplate());
 }
+render(filmListElement, createShowMoreButtonTemplate(), 'afterend');
 
 const extraFilmListsElements = siteMainElement.querySelectorAll('.films-list--extra .films-list__container');
 
