766e0a840181cd2989b5112bce0b5feec8259124
kokinz <nes-t@mail.ru>
2021-04-20 11:49:36 +0000

    fix

--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -7,13 +7,7 @@ const createSiteMenuTemplate = () => {
       <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
-  </nav>
-
-  <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`;
+  </nav>`;
 };
 
 export {createSiteMenuTemplate};
