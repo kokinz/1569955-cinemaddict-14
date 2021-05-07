035085c4d3f135405aacda96cd1c0f2016e7322d
kokinz <nes-t@mail.ru>
2021-05-08 00:45:21 +0300

    done

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import {createFilmListsTemplate} from './view/film-lists.js';
 import {createFilmCardTemplate} from './view/film-card.js';
 import {createUserRankTemplate} from './view/user-rank.js';
 import {createShowMoreButtonTemplate} from './view/show-more-button.js';
-// import {createFilmPopupTemplate} from './view/film-popup.js';
+import {createFilmPopupTemplate} from './view/film-popup.js';
 import {createMoviesCounterTemplate} from './view/movies-counter.js';
 import {generateFilm} from './mock/film.js';
 import {generateFilters} from './mock/filters.js';
@@ -28,14 +28,46 @@ render(siteHeaderElement, createUserRankTemplate(filters));
 render(siteMainElement, createSiteMenuTemplate(filters), 'afterbegin');
 render(siteMainElement, createFilmListsTemplate());
 render(footerStatistics, createMoviesCounterTemplate(films));
-// render(siteFooterElement, createFilmPopupTemplate(films[15]), 'afterend');
+render(siteFooterElement, createFilmPopupTemplate(films[15]), 'afterend');
 
 const filmListElement = siteMainElement.querySelector('.films-list__container');
 
-for (let i = 0; i < FILMS_CARD_COUNTER; i++) {
-  render(filmListElement, createFilmCardTemplate(films[i]));
+const onShowMoreButtonClick = (evt) => {
+  evt.preventDefault();
+
+  const filmsCount = films.length;
+  const lastFilmId = filmListElement.children.length;
+
+  if (lastFilmId + FILMS_CARD_COUNTER < filmsCount) {
+    for (let i = lastFilmId; i < lastFilmId + FILMS_CARD_COUNTER; i++) {
+      render(filmListElement, createFilmCardTemplate(films[i]));
+    }
+  } else {
+    for (let i = lastFilmId; i < filmsCount; i++) {
+      render(filmListElement, createFilmCardTemplate(films[i]));
+    }
+
+    const showMoreButton = siteMainElement.querySelector('.films-list__show-more');
+
+    showMoreButton.remove();
+  }
+};
+
+if (films.length <= FILMS_CARD_COUNTER) {
+  for (let i = 0; i < films.length; i++) {
+    render(filmListElement, createFilmCardTemplate(films[i]));
+  }
+} else {
+  for (let i = 0; i < FILMS_CARD_COUNTER; i++) {
+    render(filmListElement, createFilmCardTemplate(films[i]));
+  }
+
+  render(filmListElement, createShowMoreButtonTemplate(), 'afterend');
+
+  const showMoreButton = siteMainElement.querySelector('.films-list__show-more');
+
+  showMoreButton.addEventListener('click', onShowMoreButtonClick);
 }
-render(filmListElement, createShowMoreButtonTemplate(), 'afterend');
 
 const extraFilmListsElements = siteMainElement.querySelectorAll('.films-list--extra .films-list__container');
 
