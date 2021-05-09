63f8f01740cfa7e34a479ed0378373c7bc1a5f1f
kokinz <nes-t@mail.ru>
2021-05-09 20:44:12 +0300

    Дополнение к исправлениям

--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,13 +1,12 @@
+const getFiltersTemplate = (filters) => filters.map((filter) => {
+  return `<a href="#${filter.name}" class="main-navigation__item">${filter.name} <span class="main-navigation__item-count">${filter.count}</span></a>`;
+}).join(' ');
+
 const createSiteMenuTemplate = (filters) => {
-  const FiltersTemplate = filters.map((filter) => {
-    return `<a href="#${filter.name}" class="main-navigation__item">${filter.name} <span class="main-navigation__item-count">${filter.count}</span></a>`;
-  }).join(' ');
-
-
   return `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      ${FiltersTemplate}
+      ${getFiltersTemplate(filters)}
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>
--- src/view/user-rank.js
+++ src/view/user-rank.js
@@ -1,21 +1,21 @@
+const getRank = (filters) => {
+  const history = filters.find((filter) => filter.name === 'History');
+
+  switch (true) {
+    case history.count === 0:
+      return '';
+    case history.count > 0 && history.count <= 10:
+      return 'Novice';
+    case history.count > 10 && history.count <= 20:
+      return 'Fun';
+    case history.count > 20:
+      return 'Movie Buff';
+  }
+};
+
 const createUserRankTemplate = (filters) => {
-  const getRank = () => {
-    const history = filters.find((filter) => filter.name === 'History');
-
-    switch (true) {
-      case history.count === 0:
-        return '';
-      case history.count > 0 && history.count <= 10:
-        return 'Novice';
-      case history.count > 10 && history.count <= 20:
-        return 'Fun';
-      case history.count > 20:
-        return 'Movie Buff';
-    }
-  };
-
   return `<section class="header__profile profile">
-    <p class="profile__rating">${getRank()}</p>
+    <p class="profile__rating">${getRank(filters)}</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`;
 };
